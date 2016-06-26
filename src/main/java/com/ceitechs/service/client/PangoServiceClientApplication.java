4c9ade2ed0232fabeff2c52d9f04d26476aac741
Iddy Magohe <iddyiam@gmail.com>
2016-06-25 21:18:35 -0400

    initial

---
+++ src/main/java/com/ceitechs/service/client/PangoServiceClientApplication.java
@@ -0,0 +1,12 @@
+package com.ceitechs.service.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PangoServiceClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PangoServiceClientApplication.class, args);
+	}
+}
